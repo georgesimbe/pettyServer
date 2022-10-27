ea0bd44dad57489a0e95c74ce4818aa3fefd728e
George <Georgesimbe@icloud.com>
2022-10-27 18:42:34 +1030

    fixing port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
 });
 
 app.listen(3004, function () {
-  console.log(`Server is listening on port ${PORT}`)
+  console.log(`Server is listening on port ${process.env.PORT}`)
 })
 
 app.use(logger)
